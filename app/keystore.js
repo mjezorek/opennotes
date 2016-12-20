4243afc35b25908fbb26371c120becd11738f9a2
Matt Jezorek <matt@jezorek.com>
2016-12-20 10:12:33 -0800

    Just testing some minor changes

--- app/keystore.js
+++ app/keystore.js
@@ -2,10 +2,11 @@
 "use strict";
 var OpenNotes = OpenNotes || {};
 
-var OpenNotes.keystore = {
-	db: null;
-	dbName: "OpenNotesOpenNotes.keystore";
-	objectStoreName: "Keys";
+OpenNotes = {
+	keystore = {
+	db: null,
+	dbName: "OpenNotesOpenNotes.keystore",
+	objectStoreName: "Keys",
 	open: function() {
 		return new Promise(function(fulfill, reject){
 			var req = indexedDB.open(OpenNotes.keystore.dbName, 1);
@@ -42,7 +43,7 @@ var OpenNotes.keystore = {
 		});
 	},
 	saveKey: function(publicKey, privateKey, name) {
-		return new Promise(function(fulfill, reject)) {
+		return new Promise(function(fulfill, reject) {
 			if(!OpenNotes.keystore.db) {
 				OpenNotes.keystore.open();
 			}
@@ -69,58 +70,60 @@ var OpenNotes.keystore = {
 				reject(err);
 			});
 		});
-},
-getKey: function(propertyName, propertyValue) {
-	return new Promise(function(fulfill, reject) {
-		if(!OpenNotes.keystore.db) {
-			OpenNotes.keystore.open();
-		}
-		var transaction = OpenNotes.keystore.db.transasction([OpenNotes.keystore.objectStoreName], "readonly");
-		var objectStore = transaction.objectStore(OpenNotes.keystore.objectStoreName);
-		var request;
-		switch(propertyName) {
-			case "id":
-			request = objectStore.get(propertyValue); 
-			break;
-			case "name":
-			request = objectStore.index("name").get(propertyValue);
-			break;
-			case "spki":
-			request = objectStore.index("spki").get(propertyValue);
-			break;
-			default:
-			reject(new Error("No such property: " + propertyName));
-		}
-		request.onsuccess = function(evt) {
-			fulfill(evt.target.result);
-		};
-		request.onerror = function(evt) {
-			reject(evt.error);
-		};
-	});
-},
-listKeys: function() {
-	return new Promise(function(fullfill, reject) {
-		if(!OpenNotes.keystore.db) {
-			OpenNotes.keystore.open();
-		}
-		var list = [];
-		var transaction = OpenNotes.keystore.db.transaction([OpenNotes.keystore.objectStoreName], "readonly");
-		transaction.onerror = function(evt) {
-			reject(evt.error);
-		};
-		transaction.onabort = function(evt) {
-			reject(evt.error);
-		};
-		var objectStore = transaction.objectStore(OpenNotes.keystore.objectStoreName);
-		var cursor = objectStore.openCursor();
-		cursor.onsuccess = function(evt) {
-			if(evt.target.result) {
-				list.push({id: evt.target.result.key, value: evt.target.result.value});
-				evt.target.result.continue();
-			} else {
-				fulfill(list);
+	},
+	getKey: function(propertyName, propertyValue) {
+		return new Promise(function(fulfill, reject) {
+			if(!OpenNotes.keystore.db) {
+				OpenNotes.keystore.open();
 			}
-		}
-	});
+			var transaction = OpenNotes.keystore.db.transasction([OpenNotes.keystore.objectStoreName], "readonly");
+			var objectStore = transaction.objectStore(OpenNotes.keystore.objectStoreName);
+			var request;
+			switch(propertyName) {
+				case "id":
+				request = objectStore.get(propertyValue); 
+				break;
+				case "name":
+				request = objectStore.index("name").get(propertyValue);
+				break;
+				case "spki":
+				request = objectStore.index("spki").get(propertyValue);
+				break;
+				default:
+				reject(new Error("No such property: " + propertyName));
+			}
+			request.onsuccess = function(evt) {
+				fulfill(evt.target.result);
+			};
+			request.onerror = function(evt) {
+				reject(evt.error);
+			};
+		});
+	},
+	listKeys: function() {
+		return new Promise(function(fullfill, reject) {
+			if(!OpenNotes.keystore.db) {
+				OpenNotes.keystore.open();
+			}
+			var list = [];
+			var transaction = OpenNotes.keystore.db.transaction([OpenNotes.keystore.objectStoreName], "readonly");
+			transaction.onerror = function(evt) {
+				reject(evt.error);
+			};
+			transaction.onabort = function(evt) {
+				reject(evt.error);
+			};
+			var objectStore = transaction.objectStore(OpenNotes.keystore.objectStoreName);
+			var cursor = objectStore.openCursor();
+			cursor.onsuccess = function(evt) {
+				if(evt.target.result) {
+					list.push({id: evt.target.result.key, value: evt.target.result.value});
+					evt.target.result.continue();
+				} else {
+					fulfill(list);
+				}
+			}
+		});
+	}
+}
 };
